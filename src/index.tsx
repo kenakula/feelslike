9a83b71f2cc88d30673ea0806b5273dc9a617ad7
AgaevK <kenkosh@yandex.ru>
2023-06-21 09:21:27 +0300

    adds pwa

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import { createRoot } from 'react-dom/client';
 import { App } from 'app/app';
 import 'react-calendar/dist/Calendar.css';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { ThemeStoreProvider } from 'app/shared/utils';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+
+serviceWorkerRegistration.register();
 
 root.render(
   <ThemeStoreProvider>
